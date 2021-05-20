cf15884b78b6027d0fc01fbd4b1b94e69f8a4bae
María Rodríguez Gallego <82885765+MariaRodGal@users.noreply.github.com>
2021-05-20 09:04:00 +0200

    Corregir un error
    Añadir que coge sólo la rama master

--- git.sh
+++ git.sh
@@ -5,4 +5,4 @@ cd ~/repo
 git config --global user.name "MariaRodGal"
 git config --global user.email "mariarodgallego@gmail.com"
 git remote add origin https://github.com/MariaRodGal/ProyectoASIR.git
-git clone origin
+git clone origin master
