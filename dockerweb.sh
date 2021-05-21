35608d3e2212a82b721ab499ab4298d655f870be
María Rodríguez Gallego <82885765+MariaRodGal@users.noreply.github.com>
2021-05-21 15:25:13 +0200

    Update dockerweb.sh

--- dockerweb.sh
+++ dockerweb.sh
@@ -3,7 +3,8 @@
 curl -fsSL https://get.docker.com | sh
 
 #Crear directorio y variable
-mkdir $HOME/hassioweb
+mkdir ~/hassioweb
+cp -r ~/repo/ProyectoASIR/web/* ~/hassioweb
 
 #Levantar container de docker
 docker run -dit --name hassio-web -p 8080:80 -v $HOME/hassioweb/:/usr/local/apache2/htdocs/ httpd:2.4
